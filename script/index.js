31536ab2d44ada6107dea6b0d410e302fc290f32
María Bosom <100429055@alumnos.uc3m.es>
2021-11-30 12:33:13 +0100

    Móvil experience html

--- script/index.js
+++ script/index.js
@@ -6,4 +6,45 @@ $(document).ready(function() {
 			$('#nav-header-menu').hide();
 		}
 	})
+
+	// The images in the experiences are displayed using a photoroulette
+	$(".arr-left").click(function(){
+		let art = $(this).parent();
+		let objs = $(art).children('div');
+		var obj1 = $(objs).children('.obj1');
+		var obj2 = $(objs).children('.obj2');
+		var obj3 = $(objs).children('.obj3');
+		var obj4 = $(objs).children('.obj4');
+		var obj5 = $(objs).children('.obj5');
+		obj1.removeClass('obj1');
+		obj2.removeClass('obj2');
+		obj3.removeClass('obj3');
+		obj4.removeClass('obj4');
+		obj5.removeClass('obj5');
+		obj1.addClass('obj5');
+		obj2.addClass('obj1');
+		obj3.addClass('obj2');
+		obj4.addClass('obj3');
+		obj5.addClass('obj4');
+	})
+
+	$(".arr-right").click(function(){
+		let art = $(this).parent();
+		let objs = $(art).children('div');
+		var obj1 = $(objs).children('.obj1');
+		var obj2 = $(objs).children('.obj2');
+		var obj3 = $(objs).children('.obj3');
+		var obj4 = $(objs).children('.obj4');
+		var obj5 = $(objs).children('.obj5');
+		obj1.removeClass('obj1');
+		obj2.removeClass('obj2');
+		obj3.removeClass('obj3');
+		obj4.removeClass('obj4');
+		obj5.removeClass('obj5');
+		obj1.addClass('obj2');
+		obj2.addClass('obj3');
+		obj3.addClass('obj4');
+		obj4.addClass('obj5');
+		obj5.addClass('obj1');
+	})
 })
