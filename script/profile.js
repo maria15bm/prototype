99f8b70238dbc914c34efb6163e0619e1085396e
sebastianuc3m <100429139@alumnos.uc3m.es>
2021-12-06 23:28:36 +0100

    messages, profile popups y plantilla popups en common.css

--- script/profile.js
+++ script/profile.js
@@ -3,4 +3,23 @@ $(document).ready(function() {
     window.location.href = "my_exp.html";
   })
 
+  $(".exit-button").click(function(){
+    $(".exit-button").closest(".popup").css("display", "none");
+  })
+
+  $("#change-pfp-begin").click(function () {
+    $("#change-pfp-begin").closest(".popup").css("display", "none");
+    $("#change-pfp").css("display", "block");
+  })
+
+  $("#mod-but").click(function () {
+    $("#mod-but").closest(".popup").css("display", "none");
+    $("#modify-profile").css("display", "block");
+  })
+
+  $("#inter-but").click(function () {
+    $("#inter-but").closest(".popup").css("display", "none");
+    $("#change-interests").css("display", "block");
+  })
+
 });
